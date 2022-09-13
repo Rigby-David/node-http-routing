d20c6aae7bcb03340bd7799d0c653584379c6aae
David <Rigby335@hotmail.com>
2022-09-13 14:19:25 -0700

    get routes for all controllers

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-import http from "node:http";
-import app from "./lib/app.js";
+import http from 'node:http';
+import dotenv from 'dotenv';
+import app from './lib/app.js';
+
+dotenv.config();
 
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.APP_PORT || 8080;
+const hostName = process.env.APP_HOST || 'localhost';
 
-server.listen(PORT, () => {
-  console.log(server.address());
+server.listen(port, hostName, () => {
+  console.log(
+    `Server listening on: ${JSON.stringify(server.address())}`
+  );
 });
